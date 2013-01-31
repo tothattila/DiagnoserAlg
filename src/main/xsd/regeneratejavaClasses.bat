e72db763c48da27f79b93b5e5541cf59685cb9d4
unknown <eatttth@E7E4115BF03E34.ericsson.se>
2013-01-31 15:59:27 +0100

    REF: housekeeping, deleting obsolete stuff

--- src/main/xsd/regeneratejavaClasses.bat
+++ src/main/xsd/regeneratejavaClasses.bat
@@ -1,6 +1,4 @@
 @ECHO OFF
 
-CALL xjc -p org.diagnoser.model.xml.extended.trace -d ../java "extended trace.xsd"
-CALL xjc -p org.diagnoser.model.xml.compact.trace -d ../java "compact trace.xsd"
-CALL xjc -p org.diagnoser.model.xml.extended.hazid -d ../java "extended hazid.xsd"
-CALL xjc -p org.diagnoser.model.xml.compact.hazid -d ../java "compact hazid.xsd"
+CALL xjc -p org.diagnoser.model.xml.compact.trace -d ../java "trace.xsd"
+CALL xjc -p org.diagnoser.model.xml.compact.hazid -d ../java "hazid.xsd"
