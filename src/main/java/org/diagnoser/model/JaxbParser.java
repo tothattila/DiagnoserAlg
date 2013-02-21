c8e25b3a592375812b2265086981adbb55b3a02d
eatttth <attila.toth@ericsson.com>
2013-02-21 15:46:52 +0100

    ADD: parsign of directories added to the parser

--- src/main/java/org/diagnoser/model/JaxbParser.java
+++ src/main/java/org/diagnoser/model/JaxbParser.java
@@ -3,7 +3,10 @@ package org.diagnoser.model;
 import org.diagnoser.algorithm.LogPrinter;
 import org.diagnoser.model.exception.InvalidHazid;
 import org.diagnoser.model.internal.*;
+import org.diagnoser.model.internal.element.HazidElement;
 import org.diagnoser.model.internal.exception.*;
+import org.diagnoser.model.internal.parser.CellParser;
+import org.diagnoser.model.internal.parser.EventParser;
 import org.diagnoser.model.xml.compact.DirectoryLister;
 import org.diagnoser.model.xml.compact.hazid.Procedurehazidtable;
 import org.diagnoser.model.xml.compact.hazid.Row;
@@ -12,6 +15,9 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,20 +31,39 @@ import java.util.Map;
  */
 public class JaxbParser {
 
-    public Map<String,HazidTable> parseDir(final String dir) throws JAXBException, InvalidHazid {
+    public Map<String,HazidTable> parseHazidXmlDir(final String dir) throws JAXBException, InvalidHazid {
 
         List<String> xmlFiles = new DirectoryLister().listAllXmlsInDirectory(dir);
-        LogPrinter.printCaption("Beginning to parse `" + xmlFiles.size() + "` number of files from given directory `" + dir + "`");
+        LogPrinter.printCaption("Beginning to parse " + xmlFiles.size() + " HAZID XML file(s) from given directory `" + dir + "`");
         Map<String,HazidTable> result = new HashMap<String, HazidTable>();
 
         for (String s:xmlFiles) {
             LogPrinter.printMessage("Parsing file:"+s+"...");
             HazidTable simpleTable = parseHazidXml(s);
-            String newId = returnOrGenerateUniqueId(simpleTable.getId(), result.size());
-            result.put(newId, simpleTable);
-            LogPrinter.printMessage("...HAZID table " + newId + " loaded");
+            if (simpleTable==null) {
+                LogPrinter.printMessage("Parsing failure, got null from JAXB parser! Check structure of XML.");
+            }
+            result.put(simpleTable.getId(), simpleTable);
+            LogPrinter.printMessage("...HAZID table " + simpleTable.getId() + " has been loaded");
+        }
 
+        return result;
+    }
 
+    public Map<String,Trace> parseTraceDir(final String dir) throws JAXBException, CorruptTraceException, AlreadyPresentException, InvalidOutput, InvalidTraceFragment {
+        List<String> xmlFiles = new DirectoryLister().listAllXmlsInDirectory(dir);
+        LogPrinter.printCaption("Beginning to parse " + xmlFiles.size() + " nominal trace XML file(s) from given directory `" + dir + "`");
+        Map<String,Trace> result = new HashMap<String,Trace>();
+
+        for (String s:xmlFiles) {
+            LogPrinter.printMessage("Parsing file:"+s+"...");
+            Trace trace = parseTraceXml(s);
+            if (trace==null) {
+                LogPrinter.printMessage("Parsing failure, got null from JAXB parser! Check structure of XML.");
+            }
+            String newId = returnOrGenerateUniqueId(trace.getName(), result.size());
+            result.put(newId, trace);
+            LogPrinter.printMessage("...trace " + newId + " has been loaded");
         }
 
         return result;
@@ -65,7 +90,7 @@ public class JaxbParser {
 
     private Trace createInternalTraceFromCompactXmlTrace(String xmlFile, org.diagnoser.model.xml.compact.trace.Trace xmlTrace) throws CorruptTraceException, AlreadyPresentException, InvalidOutput, InvalidTraceFragment {
         int currentTime = 1;
-        Trace retTrace = new Trace(xmlTrace.getId());
+        Trace retTrace = traceFactory(xmlTrace);
 
         for (String fragment:xmlTrace.getEvent()) {
 
@@ -96,7 +121,7 @@ public class JaxbParser {
 
     private HazidTable createHazidTableFromCompactHazid(final String xmlFile, final Procedurehazidtable hazid) throws InvalidHazid {
 
-        final HazidTable table = new HazidTable(hazid.getId());
+        final HazidTable table = hazidTableFactory(hazid);
 
         for (Row row: hazid.getRow()) {
            try {
@@ -113,6 +138,34 @@ public class JaxbParser {
 
         return table;
 
+    }
+
+    private HazidTable hazidTableFactory(Procedurehazidtable hazid) {
+
+        final String hazidId;
+        if (hazid.getId() == null) {
+           hazidId="HAZID-"+generateCode();
+        }
+        else {
+           hazidId=hazid.getId();
+        }
+        return new HazidTable(hazidId);
+    }
+
+    private String generateCode() {
+        SecureRandom secRnd = new SecureRandom();
+        return new BigInteger(30,secRnd).toString(32).toUpperCase();
+    }
+
+    private Trace traceFactory(org.diagnoser.model.xml.compact.trace.Trace xmlTrace) {
+        final String traceId;
+        if (xmlTrace.getId() == null) {
+            traceId="TRACE-"+generateCode();
+        }
+        else {
+            traceId=xmlTrace.getId();
+        }
+        return new Trace(traceId,xmlTrace.getAssociatedhazid()==null?"":xmlTrace.getAssociatedhazid());
     }
 
 }
